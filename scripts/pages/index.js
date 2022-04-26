18144fe73e79d7b0216195e63653d47c8de8ed77
Jeanne Boutry <jeanne.boutry@hotmail.fr>
2022-04-26 18:37:44 +0200

    Etape 4 en cours, ajout récup id param url

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -8,12 +8,22 @@ async function getPhotographers() {
     .then((datas) => {
         let allPhotographers = datas.photographers;
         const photographers = allPhotographers.map(photographer => new Photographers(photographer))
-        console.log(photographers)
         const photographersSection = document.querySelector(".photographer_section");
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
+            document.querySelectorAll(".photograph_item").forEach(data => data.addEventListener('click', function() {
+                const test = data.src;
+                const queryString = window.location.href;
+                const newData = test.replace(queryString +'assets/photographers/', '');
+                let foundId = -1;
+                for(let i=0; i<photographers.length; i++) {
+                    if(photographers[i].portrait === newData) {
+                        foundId = i;
+                    }
+                }
+            }))
         });
     })
     .catch(err => console.log('==== error ====', err));
@@ -22,5 +32,3 @@ async function getPhotographers() {
 
 getPhotographers()
 
-
-    
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,31 @@
-//Mettre le code JavaScript lié à la page photographer.html
+
+async function getPhotographer() {
+    
+    fetch('../../data/photographers.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((datas) => {
+        const queryString = window.location.search;
+        const photographId = queryString.replace('?', '');
+        let allPhotographers = datas.photographers;
+        console.log(allPhotographers);
+        let foundId = -1;
+        for(let i=0; i<allPhotographers.length; i++) {
+            if(allPhotographers[i].id == photographId) {
+                foundId = i;
+            }
+            console.log(foundId);
+            
+        }
+        const photographer = new Photographers(allPhotographers[foundId])
+        const photographersSection = document.querySelector(".one_photographer_section");
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM2();
+        photographersSection.appendChild(userCardDOM);
+    })
+    .catch(err => console.log('==== error ====', err));
+            
+}
+
+getPhotographer();
